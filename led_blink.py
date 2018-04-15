41a091a6cd0c099384fe83f99c75ea5677905a58
tommcm <tommcm@amazon.com>
2018-04-15 20:11:07 +1000

    3 leds

--- led_blink.py
+++ led_blink.py
@@ -4,7 +4,7 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 
-chan_list = [7,11]
+chan_list = [7,11,13]
 GPIO.setup(chan_list, GPIO.OUT)
 
 # GPIO.setup(7, GPIO.OUT)
@@ -12,8 +12,14 @@ GPIO.setup(chan_list, GPIO.OUT)
 for i in range(50):
     GPIO.output(7,True)
     GPIO.output(11,False)
+    GPIO.output(13,False)
     time.sleep(1)
     GPIO.output(7,False)
     GPIO.output(11,True)
+    GPIO.output(13,False)
+    time.sleep(1)
+    GPIO.output(7,False)
+    GPIO.output(11,False)
+    GPIO.output(13,True)
     time.sleep(1)
 GPIO.cleanup()
