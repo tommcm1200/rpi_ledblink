4001414505ea44ea9d69b25af3eb4effe92af2a9
tommcm <tommcm@amazon.com>
2018-04-15 19:55:41 +1000

    second channel - 11

--- led_blink.py
+++ led_blink.py
@@ -1,12 +1,19 @@
 #import the GPIO and time package
 import RPi.GPIO as GPIO
 import time
+
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.OUT)
+
+chan_list = [7,11]
+GPIO.setup(chan_list, GPIO.OUT)
+
+# GPIO.setup(7, GPIO.OUT)
 # loop through 50 times, on/off for 1 second
 for i in range(50):
     GPIO.output(7,True)
+    GPIO.output(11,False)
     time.sleep(1)
     GPIO.output(7,False)
+    GPIO.output(11,True)
     time.sleep(1)
 GPIO.cleanup()
